d648ba3fbf07ec297f48e5149a5b523b319247bf
Kieran Nagarajah <107479122+kierannagarajah9@users.noreply.github.com>
2022-06-16 13:22:17 +0100

    Update RockPaperScissors.java

--- BasicProgrammingConcepts/src/RockPaperScissors.java
+++ BasicProgrammingConcepts/src/RockPaperScissors.java
@@ -3,13 +3,58 @@ import java.util.Scanner;
 
 public class RockPaperScissors {
     public static void main(String[] args) {
+        playGame();
+    }
+
+    public static void playGame(){
+        System.out.println("In the playgame");
+        int round = 1;
+        int userWins =0;
+        int compWins = 0;
+
         Scanner scan = new Scanner(System.in);
 
         System.out.println("How many rounds do you want to play?");
         int numRounds = scan.nextInt();
 
+        System.out.println("Number: "+numRounds);
+
         if(numRounds>=1 && numRounds<=10){
-            playGame(numRounds);
+            for(int i=1;i<=numRounds;i++) {
+
+                System.out.println("What would you like to choose, Rock(1), Paper(2) or Scissors(3)?");
+                int choice = scan.nextInt();
+                System.out.println("Player chooses " + choice);
+
+                Random randNum = new Random();
+                int compNum = randNum.nextInt(3) + 1;    //random num between 1 and 3
+                System.out.println("Computer chooses " + compNum);
+
+                if(choice == 1 & compNum == 2){
+                    compWins++;
+                }
+                else if(choice == 1 & compNum == 3){
+                    userWins++;
+                }
+                else if(choice == 2 & compNum == 1){
+                    userWins++;
+                }
+                else if(choice ==2 && compNum == 3){
+                    compWins++;
+                }
+                else if(choice ==3 && compNum == 1){
+                    compWins++;
+                }
+                else if(choice ==3 && compNum == 2){
+                    userWins++;
+                }
+
+                round++;
+            }
+
+            System.out.println("User wins: "+userWins);
+            System.out.println("Computer wins: "+compWins);
+            System.out.println("Number of ties: "+(numRounds-userWins-compWins));
         }
         else{
             System.out.println("This number of rounds is not allowed!");
@@ -21,55 +66,10 @@ public class RockPaperScissors {
         if(playAgain == 1){
             System.out.println("How many rounds do you want to play?");
             numRounds = scan.nextInt();
-            playGame(numRounds);
+            playGame();
         }
         else{
             System.out.println("Thanks for playing!");
         }
-
-
-    }
-
-    public static void playGame(int numRounds){
-        int round = 1;
-        int userWins =0;
-        int compWins = 0;
-
-        for(int i=1;i<=numRounds;i++) {
-
-            Scanner scan = new Scanner(System.in);
-            System.out.println("What would you like to choose, Rock(1), Paper(2) or Scissors(3)?");
-            int choice = scan.nextInt();
-            System.out.println("Player chooses " + choice);
-
-            Random randNum = new Random();
-            int compNum = randNum.nextInt(3) + 1;    //random num between 1 and 3
-            System.out.println("Computer chooses " + compNum);
-
-            if(choice == 1 & compNum == 2){
-                compWins++;
-            }
-            else if(choice == 1 & compNum == 3){
-                userWins++;
-            }
-            else if(choice == 2 & compNum == 1){
-                userWins++;
-            }
-            else if(choice ==2 && compNum == 3){
-                compWins++;
-            }
-            else if(choice ==3 && compNum == 1){
-                compWins++;
-            }
-            else if(choice ==3 && compNum == 2){
-                userWins++;
-            }
-
-            round++;
-        }
-
-        System.out.println("User wins: "+userWins);
-        System.out.println("Computer wins: "+compWins);
-        System.out.println("Number of ties: "+(numRounds-userWins-compWins));
     }
 }
